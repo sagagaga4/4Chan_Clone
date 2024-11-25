9ad85df2f4036394263a3f1b4a3b5ba02b097cf7
‪Sagi‏ <57183982+sagagaga4@users.noreply.github.com>
2024-11-25 23:15:27 +0200

    Add files via upload

---
+++ src/Boards/Pop.tsx
@@ -0,0 +1,440 @@
+import React from 'react';
+import './Pop.css';
+
+const Pop: React.FC = () => {
+    return (
+        <div className="board">
+        <div className="boardbar">
+        <h3>Popular Threads</h3>
+        </div>
+        <div className="column_1">
+        <h5>
+            Comics & Cartoons
+        </h5>
+        <ul>
+        <li>
+        <a className="boardlink" href="#">
+        Anime & Manga
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Anime/Cute
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Anime/Wallpapers
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Mecha
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Cosplay & EGL
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Cute/Male
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Flash
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Transportation
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Otaku Culture
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Virtual Youtubers
+        </a>
+        </li>                
+        </ul>
+        <h5 style={{ textDecoration: 'underline', display: 'inline' }}>
+        Video Games
+        </h5>
+        <ul>
+        <li>
+        <a className="boardlink" href="#">
+        Video Games
+        </a>
+        </li>
+
+        <li>
+        <a className="boardlink" href="#">
+        Video Games Generals
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Video Games/Multiplayer
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Video Games/Mobile
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Pokemon
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Retro Games
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Video Games/RPG
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Video Games/Strategy
+        </a>
+        </li>
+        </ul>
+        </div>
+        <div className="column_1">
+        <h5 style={{ textDecoration: 'underline', display: 'inline' }}>
+        Interests
+        </h5>
+        <ul>
+        <li>
+        <a className="boardlink" href="#">
+        Comic & Cartoons
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Technology
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Television & Film
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Weapons
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Auto
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Animals & Nature
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Traditional Games
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Sports
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Extreme Sports
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Professional Wresteling
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Science & Math
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        History & Humanities
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        International
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Outdoors
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Toys
+        </a>
+        </li>
+        </ul>
+        </div>
+        <div className="column_1">
+        <h5 style={{ textDecoration: 'underline', display: 'inline' }}>
+        Creative
+        </h5>
+        <ul>
+        <li>
+        <a className="boardlink" href="#">
+        Oekaki
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Papercraft & Origami
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Photography
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Food & Cooking
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Artwork/Critique
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Wallpapers/General
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Literature
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Music
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Fashion
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        3DCG
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Graphic Design
+        </a>
+        </li>                   <li>
+        <a className="boardlink" href="#">
+        Do-It-Yourself
+        </a>
+        </li>                   <li>
+        <a className="boardlink" href="#">
+        Worksafe GIF
+        </a>
+        </li>                   <li>
+        <a className="boardlink" href="#">
+        Quests
+        </a>
+        </li>
+        </ul>
+        </div>
+        <div className="column_1">
+        <h5 style={{ textDecoration: 'underline', display: 'inline' }}>
+        Other
+        </h5>
+        <ul>
+        <li>
+        <a className="boardlink" href="#">
+        Business & Finance
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Travel
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Fitness
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Paranormal
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        advice
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        LGBT
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Pony
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Current News
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Workstate Requests
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Very Important Posts
+        </a>
+        </li>
+        </ul>
+        <h5 style={{ textDecoration: 'underline', display: 'inline' }}>
+        Misc
+        </h5>
+        <ul>
+        <li>
+        <a className="boardlink" href="#">
+        Random
+        </a>
+        </li>
+
+        <li>
+        <a className="boardlink" href="#">
+        ROBOT9001
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Politically Incorrect
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        International/Random
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Cams & Meetups
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Shit 4chan Says
+        </a>
+        </li>
+        </ul>
+        </div>
+        <div className="column_1">
+        <h5 style={{ textDecoration: 'underline', display: 'inline' }}>
+        Adult 
+        </h5>
+        <ul>
+        <li>
+        <a className="boardlink" href="#">
+        Sexy Beautiful
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Hardcore
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Handsome Men
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Hentai
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Ecchi
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Yuri
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Hentai/Alternative
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Yaoi
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Torrents
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        High Resolutions
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Adult GIF
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Adult Cartoons
+        </a>
+        </li>
+        <li>
+        <a className="boardlink" href="#">
+        Adult Requests
+        </a>
+        </li>
+        </ul>
+        </div>
+
+
+        </div>
+    );
+};
+
+export default Pop;
